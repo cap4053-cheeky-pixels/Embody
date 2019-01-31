b0021c11b4c4f1169587c7503f54a04af70988a7
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-01-31 17:06:28 -0500

    Add door animations and script

---
+++ Assets/Scripts/DoorMechanism.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMechanism : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            ChangeDoorState("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(doorOpen)
+        {
+            doorOpen = false;
+            ChangeDoorState("Close");
+        }
+    }
+
+    void ChangeDoorState(string state)
+    {
+        animator.SetTrigger(state);
+    }
+}
