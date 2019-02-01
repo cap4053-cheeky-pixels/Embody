8f57abb2b43ca7bbe4a35dfddc64847adcf1c8ba
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-01 15:14:20 -0500

    Add Entity base class and Player subclass.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public override void Move()
+    {
+        // TODO code
+    }
+
+    public override void Attack()
+    {
+        // TODO code
+    }
+
+    public override void ChangeHealth(int amount)
+    {
+        // TODO code
+    }
+}
