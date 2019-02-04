0d2c8a224778cf5406393bd608128e7258c17700
klein1992 <ejw695@gmail.com>
2019-02-04 15:32:18 -0500

    Added an Avoidant Enemy
    Enemy will seek out a designated corner from which it will spawn additional enemies. Script was added where enemy successfully moves to corner. Spawning enemy mechanic will be added later

---
+++ Assets/Scripts/AvoidantEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidantEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform target;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Corner").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        if(target.position.magnitude - transform.position.magnitude > .1f)
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+}
