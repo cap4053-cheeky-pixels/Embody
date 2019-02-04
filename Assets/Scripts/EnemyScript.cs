d8423a0b03dbb46ecff6f351b4ee71d89384669e
klein1992 <ejw695@gmail.com>
2019-02-04 12:44:09 -0500

    Added Sandbox Scene with Simple Enemy Movement
    Enemy will track player movement and follow it, colliding with it. No action or event is triggered at this time. Created a sandbox scene (Ed's) which showcases this.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
