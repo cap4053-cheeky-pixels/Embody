c0aac5df5e603d4626bc971b340a4f847bad6940
klein1992 <ejw695@gmail.com>
2019-02-04 15:58:02 -0500

    Added Spawnable enemies
    Script added to spawn enemies once the spawner enemy has reached the position from which it will spawn enemies. These enemies will then seek out the player.

--- Assets/Scripts/AvoidantEnemy.cs
+++ Assets/Scripts/AvoidantEnemy.cs
@@ -8,6 +8,7 @@ public class AvoidantEnemy : MonoBehaviour
 
     private Transform target;
     public float speed = 5.0f;
+    public bool spawned = false;
 
     void Start()
     {
@@ -18,8 +19,17 @@ public class AvoidantEnemy : MonoBehaviour
     void Update()
     {
         transform.LookAt(target);
-        if(target.position.magnitude - transform.position.magnitude > .1f)
-        transform.position += transform.forward * speed * Time.deltaTime;
+        if (target.position.magnitude - transform.position.magnitude > .1f)
+            transform.position += transform.forward * speed * Time.deltaTime;
+        else
+        {
+            if (!spawned)
+            {
+                Instantiate(GameObject.Find("Enemy"), GameObject.Find("FlySpawnPoint").transform.position, Quaternion.identity);
+                Instantiate(GameObject.Find("Enemy"), GameObject.Find("FlySpawnPoint2").transform.position, Quaternion.identity);
+                spawned = true;
+            }
+        }
     }
 
 }
