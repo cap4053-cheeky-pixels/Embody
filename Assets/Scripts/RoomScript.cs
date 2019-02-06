8e05bb7e06d9b44ec62adfebc56828f9e2a34133
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-06 14:08:47 -0500

    Add scripts for spawning enemies in a room.

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    SpawnScript spawner;
+    List<GameObject> enemies;
+    int numEnemies;
+
+    // TODO somehow keep track of current list of enemies, not just original... how do we know when an enemy dies?
+
+    private void Start()
+    {
+        spawner = gameObject.transform.Find("SpawnPoints").gameObject.GetComponent<SpawnScript>();
+        enemies = spawner.SpawnEnemies();
+        numEnemies = enemies.Count;
+    }
+}
---
+++ Assets/Scripts/Spawning/SpawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    /* Loops through all children SpawnPoint objects and spawns enemies at those points.
+     * Returns a list of all spawned enemies.
+     */ 
+    public List<GameObject> SpawnEnemies()
+    {
+        List<GameObject> enemiesSpawned = new List<GameObject>();
+
+        // Loop through each spawn point
+        foreach(Transform child in transform)
+        {
+            SpawnPoint spawnPoint = child.gameObject.GetComponent<SpawnPoint>();
+            GameObject enemyToSpawn = spawnPoint.enemyPrefab;
+
+            // If an enemy is set to spawn at that point, go ahead and instantiate it
+            if (enemyToSpawn != null)
+            {
+                Instantiate(enemyToSpawn, spawnPoint.transform.position, Quaternion.identity);
+                enemiesSpawned.Add(enemyToSpawn);
+            }
+        }
+
+        return enemiesSpawned;
+    }
+}
