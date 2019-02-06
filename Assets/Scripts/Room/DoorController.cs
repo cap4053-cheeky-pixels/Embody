a54c6e85f9a92540ad0afc7cde76e7d06f1f96e4
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-06 14:33:25 -0500

    Add DoorController script to Door prefab

---
+++ Assets/Scripts/Room/DoorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    /* Opens the Door object this script is associated with */
+    public void Open()
+    {
+        ChangeDoorState("Open");
+    }
+
+    /* Closes the Door object this script is associated with */
+    public void Close()
+    {
+        ChangeDoorState("Close");
+    }
+
+    /* Changes the associated Door object's state via its animator component */
+    void ChangeDoorState(string state)
+    {
+        animator.SetTrigger(state);
+    }
+}
---
+++ Assets/Scripts/Room/RoomScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    public List<GameObject> doors;
+
+    private SpawnScript spawner;
+    private List<GameObject> enemies;
+    private int numEnemies;
+
+    // TODO somehow keep track of current list of enemies, not just original... how do we know when an enemy dies?
+
+    private void Start()
+    {
+        spawner = gameObject.transform.Find("SpawnPoints").gameObject.GetComponent<SpawnScript>();
+        enemies = spawner.SpawnEnemies();
+        numEnemies = enemies.Count;
+    }
+
+    /* Called when any other collision object enters this Room. Used to detect when the player
+     * enters the room. If there are currently enemies, it will lock all doors.
+     */ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" && numEnemies != 0)
+        {
+            // Lock all doors behind the player if there are still enemies
+            foreach(GameObject door in doors)
+            {
+                DoorController doorController = door.GetComponent<DoorController>();
+                doorController.Close();
+            }
+        }
+    }
+}
