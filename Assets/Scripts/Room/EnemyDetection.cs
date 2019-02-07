b4be37cfec4369793946625d45acb0860e9c292e
Jason Hackleman <hackleman.jason@gmail.com>
2019-02-06 18:24:01 -0600

    new method for counting enemies based on location.  RoomScript now works with multiple rooms

---
+++ Assets/Scripts/Room/EnemyDetection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+
+    public Vector3 center;
+    public GameObject room;
+
+    Collider[] colliders;
+    private int enemies;
+    private int radius;
+
+    // Returns the number of enemy objects within a set radius
+
+    public int EnemyCount(GameObject room)
+    {
+
+        center = room.transform.position;
+        radius = 14;
+        enemies = 0;
+
+
+        colliders = Physics.OverlapSphere(center, radius);
+        foreach (Collider enemy in colliders)
+        {
+            if (enemy.gameObject.tag == "Enemy")
+            {
+                enemies++;
+            }
+        }
+        return enemies;
+    }
+}
--- Assets/Scripts/Room/RoomScript.cs
+++ Assets/Scripts/Room/RoomScript.cs
@@ -6,9 +6,12 @@ public class RoomScript : MonoBehaviour
 {
     // To be assigned via the Unity editor
     public List<GameObject> doors;
+    public GameObject currentRoom;
 
+    private EnemyDetection detection;
     private SpawnScript spawner;
     private List<GameObject> enemies;
+    private WaitForSeconds wait;
     private int numEnemies = 0;
 
     // TODO somehow keep track of current list of enemies, not just original... how do we know when an enemy dies?
@@ -21,22 +24,45 @@ public class RoomScript : MonoBehaviour
         Debug.Log("Number of enemies spawned = " + numEnemies);
     }
 
+    void Start()
+    {
+        wait = new WaitForSeconds(2.0f);
+        detection = currentRoom.GetComponent<EnemyDetection>();
+        numEnemies = detection.EnemyCount(currentRoom);
+    }
+
+
+
     /* Called when any other collision object enters this Room. Used to detect when the player
      * enters the room. If there are currently enemies, it will lock all doors.
      */
-    private void OnCollisionEnter(Collision collision)
+    private IEnumerator OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+       if (collision.gameObject.tag == "Player")
         {
+
             // Enemies remaining
             if (numEnemies != 0)
             {
+
                 // Lock all doors behind the player if there are still enemies
                 foreach (GameObject door in doors)
                 {
                     DoorController doorController = door.GetComponent<DoorController>();
                     doorController.Close();
                 }
+                /* Updates numEnemies every 'wait' seconds */
+                while (numEnemies != 0)
+                {
+                    numEnemies = detection.EnemyCount(currentRoom);
+                    yield return wait;
+                }
+                foreach (GameObject door in doors)
+                {
+                    DoorController doorController = door.GetComponent<DoorController>();
+                    doorController.Open();
+                }
+
             }
             // No more enemies
             else
@@ -48,6 +74,7 @@ public class RoomScript : MonoBehaviour
                     doorController.Open();
                 }
             }
+
         }
     }
 }
