c3f6afa4a24726cac659ae9a7d2e35cbfc182afb
Jazy Llerena <jazy@jazyserver.com>
2019-02-07 19:10:50 -0500

    Create a weapon controller with a basic weapon
    I split the ProjectileLauncher script in such a way to extract the
    firing logic out of the launcher script. That way the WeaponController
    will be in the player, and the IWeapon will be in a gameobject within
    the player, which can be swapped out during possession. This also allows
    enemies to use the same firing logic without having to alter it
    specially for them.

---
+++ Assets/Scripts/Weapons/BasicWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicWeapon : MonoBehaviour, IWeapon
+{
+    public GameObject projectile;
+    public float speed;
+    public float interval;
+    public float projectileLifetime;
+    public float damage;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public void Fire()
+    {
+        if (timer > interval)
+        {
+            Debug.Log("Blap");
+            Vector3 spawnPos =
+                new Vector3(0, 1.5f, 0) + transform.position + transform.forward * 1;
+            GameObject projectileInstance =
+                Instantiate(projectile, spawnPos, transform.rotation);
+
+            Projectile pro = projectileInstance.GetComponent<Projectile>();
+            pro.velocity = transform.forward * speed;
+            pro.damage = damage;
+
+            Destroy(projectileInstance, projectileLifetime);
+
+            timer = 0;
+        }
+    }
+}
