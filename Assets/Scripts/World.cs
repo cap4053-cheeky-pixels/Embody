9cfae131ee6103a401c21141932ea6e541110823
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-02-09 16:06:02 -0500

    Add scene loading on death. Add pause functionality.

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    Player player;
+    bool paused;
+
+    void Start()
+    {
+        paused = false;
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(paused)
+            {
+                Time.timeScale = 1;
+                player.SetEnabled(true);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                player.SetEnabled(false);
+            }
+            
+            paused = !paused;
+        }
+    }
+}
